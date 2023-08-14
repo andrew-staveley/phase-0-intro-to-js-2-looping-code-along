69aaf9607dae6a5649c54cb1deb7e583322f970e
Andrew <andrewstaveley14@gmail.com>
2023-08-13 23:09:08 -0700

    Completed assignmnet.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+const attendees = [
+    "Andrew",
+    "Libby",
+    "Kayleen",
+    "Kellie",
+    "Sabrin",
+    "Nadia",
+];
+
+const description = "Poopy";
+
+function writeCards(name, gift) {
+    let cardCollection = [];
+    for (let counter = 0; counter < name.length; counter++) {
+        console.log(`Thank you, ${name[counter]}, for the wonderful ${gift} gift!`);
+        cardCollection.push(`Thank you, ${name[counter]}, for the wonderful ${gift} gift!`);
+    }
+    return cardCollection;
+}
+
+writeCards(attendees, description);
+
+function countDown(count) {
+    let timer = count;
+    while (timer >= 0) {
+    console.log(timer--);
+    }
+
+}
+
+countDown(10);
+
